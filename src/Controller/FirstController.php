3a9fbcea1f93a9ce71ab27f8f508fab63387ff8b
jawherr <jawa.kalel@gmail.com>
2023-11-17 19:40:29 +0100

    Session and First Controller

---
+++ src/Controller/FirstController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FirstController extends AbstractController
+{
+    #[Route('/first', name: 'first')]
+    public function index(): Response
+    {
+        // chercher au la base de données vos users
+        return $this->render('first/index.html.twig', [
+            'name' => 'Kallel',
+            'firstname' => 'Jawher'
+        ]);
+    }
+    #[Route('/sayHello/{name}/{firstname}', name: 'say.hello')]
+    public function sayHello(Request $request, $name, $firstname): Response
+    {
+        return $this->render('first/hello.html.twig', [
+            'nom' => $name,
+            'prenom' => $firstname
+        ]);
+    }
+}
